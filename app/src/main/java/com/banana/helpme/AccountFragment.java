8870216cd8193692f81c7ba40c3e5559ebf574a6
Nicholas Kevin <nikokevin29@gmail.com>
2019-09-15 19:09:02 +0700

    ButtonImage Logout

--- app/src/main/java/com/banana/helpme/AccountFragment.java
+++ app/src/main/java/com/banana/helpme/AccountFragment.java
@@ -2,19 +2,36 @@ package com.banana.helpme;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountFragment extends Fragment {
+
+    private ImageButton mlogout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_account_fragment, container, false);
+        mlogout = (ImageButton) view.findViewById(R.id.imgLogout);
+        mlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getActivity(),Login.class);
+                startActivity(i);
+                getActivity().finish();
+            }
+        });
         return view;
+
     }
 }
